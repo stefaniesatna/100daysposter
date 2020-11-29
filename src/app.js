e2fcb2456cf86420a8b310c20235ecd8561cec34
Stefanie Satna <satnastefani@Stefanies-MacBook.local>
2020-11-29 17:12:15 +0000

    give it a message

---
+++ src/app.js
@@ -0,0 +1,34 @@
+console.log("something")
+const backgroundColour = [
+    document.getElementById("background-colour-white"),
+    document.getElementById("background-colour-black"),
+    document.getElementById("background-colour-orange"),
+    document.getElementById("background-colour-green"), 
+];
+const customBackgroundColour = document.getElementById("colour-code");
+
+function backgroundColourChanged() {
+    customBackgroundColour.value = "";
+}
+
+for (let i = 0; i < backgroundColour.length; i++){
+    backgroundColour[i].addEventListener("change", backgroundColourChanged)
+}
+function whatColourIsSelected(){
+    for (let i = 0; i < backgroundColour.length; i++){
+        if (backgroundColour[i].checked){
+            return backgroundColour[i].value;
+        }
+    }
+}
+
+customBackgroundColour.addEventListener("input", clearColourSelection);
+
+function clearColourSelection(){
+    for (let i = 0; i < backgroundColour.length; i++){
+        if (backgroundColour[i].checked){
+            backgroundColour[i].checked = false;
+        }
+    }
+}
+
